c4bee0958b79fdfb44e5f8e45472cf8b6a1e929e
Sahil <sahils.1997.s@gmail.com>
2020-07-10 23:21:52 +0530

    setup.py dependencies added

--- setup.py
+++ setup.py
@@ -4,19 +4,27 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="KMeansImage", # Replace with your own username
+    name="KMeansImage", 
     version="0.69",
     author="Sahil",
     author_email="sahils.1997.s@gmail.com",
     description="A K-Means based image compressor. Consists interactive widgets for Jupyter Notebook",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="",
+    url="https://github.com/imsahil007/KMeansImage",
+    download_url="https://github.com/imsahil007/KMeansImage/blob/master/dist/KMeansImage-0.69.tar.gz",
+    install_requires=[            # I get to this in a second
+          'scikit-image',
+          'ipywidgets',
+          'matplotlib',
+          'scikit-learn',
+          'numpy',
+      ],
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
+    python_requires='>=3.0',
 )
